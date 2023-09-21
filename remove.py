bd03cd9c7faec7cf6936701facf56784e8ee89ec
kourosh shafi <kouroshshafi@users.noreply.github.com>
2023-09-20 23:25:15 -0700

    Update remove.py

--- remove.py
+++ remove.py
@@ -1,27 +1,20 @@
+#pip install PyPDF2
 from PyPDF2 import PdfReader, PdfWriter
 
 def removeWatermark(input_fname: str, output_fname: str):
-
     with open(input_fname, "rb") as input_file, open(output_fname, "wb") as output_file:
-
         reader = PdfReader(input_file)
         writer = PdfWriter()
-
         for n in range(len(reader.pages)):
-
             page = reader.pages[n]
             del page["/Contents"][-1]
-
             writer.add_page(page)
-
         writer.write(output_file)
 
 if __name__ == "__main__":
-
     import sys
-
     if len(sys.argv) != 3:
-        raise RuntimeError("Arguments not correct!")
+        raise RuntimeError("Program accepts only two arguments.")
 
     removeWatermark(sys.argv[1], sys.argv[2])
 
